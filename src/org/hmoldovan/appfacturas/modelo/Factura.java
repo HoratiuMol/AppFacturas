5000ae1b3a4be8ad9fda1d1d8d3922e338b137fd
HoratiuMol <horatiu.moldovan93@gmail.com>
2023-02-14 23:55:27 +0100

    Initial commit

---
+++ src/org/hmoldovan/appfacturas/modelo/Factura.java
@@ -0,0 +1,63 @@
+package org.hmoldovan.appfacturas.modelo;
+
+import java.util.Date;
+
+public class Factura {
+
+    private int folio;
+    private String descripcion;
+    private Date fecha;
+    private Cliente cliente;
+    private ItemFactura[] items;
+    private int indiceItems;
+    public static final int MAX_ITEMS=12;
+    private static int ultimoFolio;
+
+    public Factura(String descripcion, Cliente cliente) {
+        this.descripcion = descripcion;
+        this.cliente = cliente;
+        this.items = new ItemFactura[MAX_ITEMS];
+        this.folio= ++ultimoFolio;//con este pre-incremento nos aseguramos que comience en "1"
+        this.fecha= new Date();//la factura se crea por defecto con la fecha actual
+    }
+
+    public int getFolio() {
+        return folio;
+    }
+
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public ItemFactura[] getItems() {
+        return items;
+    }
+
+    public void addItemFactura(ItemFactura item){
+
+        if(indiceItems < MAX_ITEMS) {
+            this.items[indiceItems++] = item;
+        }
+    }
+}
---
+++ src/org/hmoldovan/appfacturas/modelo/ItemFactura.java
@@ -0,0 +1,2 @@
+package org.hmoldovan.appfacturas.modelo;public class ItemFactura {
+}
